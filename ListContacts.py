235343a8fd1b8be571cc76afe4e1b10c2e96924c
Lilliancz <12296128+Lilliancz@users.noreply.github.com>
2019-10-03 11:23:04 -0400

    GAE edits

--- ListContacts.py
+++ ListContacts.py
@@ -1,3 +1,15 @@
+
+# [START gae_python37_app]
+from flask import Flask
+
+
+# If `entrypoint` is not defined in app.yaml, App Engine will look for an app
+# called `app` in `main.py`.
+app = Flask(__name__)
+
+
+@app.route('/')
+
 import requests
 import json
 import pandas as pd
@@ -47,3 +59,11 @@ final = final.drop(
 
 # save to csv
 final.to_csv('rest.csv')
+
+
+if __name__ == '__main__':
+    # This is used when running locally only. When deploying to Google App
+    # Engine, a webserver process such as Gunicorn will serve the app. This
+    # can be configured by adding an `entrypoint` to app.yaml.
+    app.run(host='127.0.0.1', port=8080, debug=True)
+# [END gae_python37_app]
