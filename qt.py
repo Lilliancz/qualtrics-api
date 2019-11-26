a464df0dc45bb2062a06be75379f67ae82287b30
Lilliancz <12296128+Lilliancz@users.noreply.github.com>
2019-11-26 15:03:07 -0500

    update qt.py with calendar to choose start date

--- qt.py
+++ qt.py
@@ -4,42 +4,82 @@ import json
 import pandas as pd
 import csv
 import time
-from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLabel
-from PyQt5.QtCore import pyqtSlot
+from datetime import datetime
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLabel, QCalendarWidget
+from PyQt5.QtCore import pyqtSlot, QDate
 import os
 
 
 class App(QWidget):
+    global currentYear, currentMonth, currentDay
+
+    currentMonth = datetime.now().month
+    currentYear = datetime.now().year
+    currentDay = datetime.now().day
 
     def __init__(self):
         super().__init__()
         self.title = 'Lending Game Download CSV'
         self.left = 100
         self.top = 100
-        self.width = 700
+        self.width = 400
         self.height = 400
         self.initUI()
 
     def initUI(self):
         self.setWindowTitle(self.title)
         self.setGeometry(self.left, self.top, self.width, self.height)
+
+        self.labelCalendar = QLabel(self)
+        self.labelCalendar.setText('Starting on which date would you like to pull data?')
+        self.labelCalendar.move(20, 20)
+        self.labelCalendar.adjustSize()
+
+        self.calendar = QCalendarWidget(self)
+        self.calendar.move(20, 40)
+        self.calendar.setGridVisible(True)
+        self.calendar.setSelectedDate(QDate(currentYear, currentMonth, currentDay))
+        self.calendar.clicked.connect(self.showDate)
+
+        self.labelC = QLabel(self)
+        date = self.calendar.selectedDate()
+        self.labelC.setText(date.toString())
+        self.labelC.resize(100,20)
+        self.labelC.move(20, 240)
+
         self.labelA = QLabel(self)
         self.labelA.setText('Click the button below to get lending game data')
-        self.labelA.move(120, 100)
+        self.labelA.move(20, 300)
         self.labelA.adjustSize()
         self.labelB = QLabel(self)
-        button = QPushButton('Click to get CSV', self)
-        button.setToolTip('Get CSV here')
-        button.move(250, 200)
-        button.clicked.connect(self.on_click)
+        self.labelB.move(20, 320)
+        self.labelDate = QLabel(self)
+        self.labelDate.move(20, 340)
+        self.labelDate.resize(100, 20)
+
+        self.pushButton = QPushButton('Click to get CSV', self)
+        self.pushButton.setToolTip('Get CSV here')
+        self.pushButton.move(20, 340)
+        self.pushButton.clicked.connect(self.on_click)
         self.show()
-		
+
+    def showDate(self, date):
+        self.labelC.setText(date.toString())
+
     @pyqtSlot()
     def on_click(self):
 
-        # you must specify the api token and mailing list ID in order for this code to work. 
-	# I haven't added any user input here since this QT version is for users who don't 
-	# have access to the token or IDs
+        self.labelA.setText('Done! Please check the directory for your CSV output!')
+        self.labelA.adjustSize()
+        self.labelB.setText('Please close this window and the command prompt window.')
+        self.labelB.adjustSize()
+        self.pushButton.hide()
+        date = self.calendar.selectedDate().toString("yyyyMMdd")
+        date = int(date)
+        #self.labelDate.setText(date)
+
+
+        # must specify
         apiToken = ""
         mailingListID = ""
         fileFormat = "csv"
@@ -80,20 +120,16 @@ class App(QWidget):
             columns=["id", "firstName", "embeddedData", "lastName", "email", "language", "unsubscribed", "responseHistory",
                      "emailHistory", "A_Info", "B_Info", "C_Info", "D_Info", "E_Info", "F_Info", "G_Info", "H_Info",
                      "I_Info",
-                     "J_Info", "DateTaken", "TriggerResponseID", "TriggerSurveyID"])
+                     "J_Info", "TriggerResponseID", "TriggerSurveyID"])
 
         # save to csv
         timestr = time.strftime("%Y%m%d-%H%M%S")
+        final['DateTaken'] = final['DateTaken'].astype(int)
+        final = final[final.DateTaken >= date]
         final.to_csv('lendingGameData'+timestr+".csv")
         os.remove("temp.csv")
 		
-        self.labelA.setText('Done! Please check the directory for your CSV output!')
-        self.labelA.move(100, 100)
-        self.labelA.adjustSize()
-        self.labelB.setText('Please close this window and the command prompt window.')
-        self.labelB.move(50, 300)
-        self.labelB.adjustSize()
-
+		
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     ex = App()
@@ -101,4 +137,3 @@ if __name__ == '__main__':
 
 # used pyinstaller to get this to be added as a single exe file for
 # non-python users to run code
-# pyinstaller qt.py -F --onefile
